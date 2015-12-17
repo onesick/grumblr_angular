593e79194833caf9c41657d1fac99cdf59f20e11
Wonsik Jeong <wsjeong21@hotmail.com>
2015-12-17 10:03:10 -0500

    init

---
+++ js/grumbles/grumbles.factory.js
@@ -0,0 +1,14 @@
+"use strict";
+
+(function(){
+  angular
+  .module("grumbles")
+  .factory("GrumbleFactory", [
+    "$resource",
+    FactoryFunction
+  ])
+
+  function FactoryFunction($resource){
+    return $resource("http://localhost:3000/grumbles/:id");
+  };
+}());
